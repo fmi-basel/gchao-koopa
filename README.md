2efcfa68cae047b2f8dd2f582e9a908be1c61c44
Bastian Eichenberger <bastian@eichenbergers.ch>
2022-07-14 14:02:13 +0200

    Add license and setup file

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
-# cuddly-octo-enigma
+# Koopa
 
-* Change config file
-* Activate conda environment
-* Start actual process: `nice -n19 python -m luigi --module merge Merge --workers ?? --local-scheduler` (8-16)
+Keenly optimized obliging picture analysis - Koopa is a luigi-pipeline based workflow to analyze cellular microscopy data of varying types - specializing on single particle analyses. The key features include:
+
+* Preprocessing
+  * For chromatic and camera alignment
+  * Z-projection
+  * Frame selection
+* Spot methods
+  * 2D/3D detection using `deepBlink` models
+  * Tracking along time
+  * Colocalization between channels
+* Segmentation
+  * Cellular segmentation (nuclei / cytoplasm) using `cellpose` models
+  * Secondary segmentation (cytoplasm / nuclei) using mathematical filters
+  * Other segmentation using mathematical filters or `segmentation_models` models
+
+## Installation
+
+Koopa can be installed using python's packaging index pypi - and has been tested for python versions 3.7-3.9.
+
+```bash
+pip install koopa
+```
+
+## Usage
+
+Once installed, koopa can be accessed as a simple command line interface using two key commands:
+
+```bash
+# To create the config file which will have to be edited
+koopa --create-config
+
+# To run the actual workflow
+koopa --config <path-to-config-file> --workers <number-of-workers>
+```
+
+## Functional Overview
+
+Koopa is composed of a few number of luigi tasks that are connected.
 
 ## TODO
 
-* [ ] Add colocalization for 3D case
 * [ ] Add alignment for multiple channels
+* [ ] Add colocalization for 3D case
+* [ ] Add functional overview
+* [ ] Add option to measure distance of spot from nuclear envelope
+* [ ] Check cfg file and overwrite on changes?
+* [ ] Create folders while using - not at the beginning
---
+++ setup.py
@@ -0,0 +1,64 @@
+"""Setup file for pypi package called koopa."""
+
+# python setup.py sdist
+# twine upload dist/latest-version.tar.gz
+
+import textwrap
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    # Description
+    name="koopa",
+    version="0.0.1",
+    license="MIT",
+    description="Workflow for analysis of cellular microscopy data.",
+    long_description_content_type="text/plain",
+    long_description=textwrap.dedent(""""""),
+    # Installation
+    python_requires=">=3.7",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "cellpose",
+        "deepblink",
+        "luigi",
+        "matplotlib",
+        "numpy",
+        "pandas",
+        "scipy",
+        "skimage",
+        "tifffile==2020.09.03",
+        "torch",
+        "tqdm",
+        "trackpy",
+    ],
+    entry_points={"console_scripts": ["koopa = koopa.cli:main"]},
+    # Metadata
+    author="Bastian Eichenberger",
+    author_email="bastian@eichenbergers.ch",
+    url="https://github.com/bbquercus/koopa/",
+    project_urls={
+        "Documentation": "https://github.com/BBQuercus/koopa/wiki",
+        "Changelog": "https://github.com/BBQuercus/koopa/releases",
+        "Issue Tracker": "https://github.com/bbquercus/koopa/issues",
+    },
+    keywords=["biomedical", "bioinformatics", "image analysis"],
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Utilities",
+    ],
+)
