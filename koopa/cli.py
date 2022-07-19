85f82a6741a380e5b2278b3a0ee3f8e30e8a6423
Bastian Eichenberger <bastian@eichenbergers.ch>
2022-07-19 11:20:28 +0200

    Set up proper logging to file and stdout

--- koopa/cli.py
+++ koopa/cli.py
@@ -3,6 +3,7 @@
 import argparse
 import configparser
 import io
+import logging
 import os
 import sys
 
@@ -38,18 +39,49 @@ CONFIGS = [
 
 def _parse_args():
     """Basic argument parser."""
-    parser = argparse.ArgumentParser(prog="Koopa", description="")
+    parser = argparse.ArgumentParser(
+        prog="Koopa",
+        description="Workflow for analysis of cellular microscopy data.",
+        add_help=False,
+    )
     parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        required=True,
+        help="Path to luigi configuration file.",
+    )
+    parser.add_argument(
+        "-w",
+        "--workers",
+        type=int,
+        default=4,
+        help="Number of parallel luigi workers to spawn. [default: 4]",
+    )
+    parser.add_argument(
+        "-cc",
         "--create-config",
         action="store_true",
         help="Create empty configuration file to be passed to `--config`",
     )
-    parser.add_argument("--config", type=str, help="Path to luigi configuration file.")
     parser.add_argument(
-        "--workers", type=int, help="Number of parallel luigi workers to spawn."
+        "-h",
+        "--help",
+        action="help",
+        default=argparse.SUPPRESS,
+        help="Show this message.",
     )
-    args = parser.parse_args()
-    return args
+    parser.add_argument(
+        "-V",
+        "--version",
+        action="version",
+        version="%(prog)s 0.0.1",
+        help="Show %(prog)s's version number.",
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+        parser.exit(0)
+    return parser.parse_args()
 
 
 def create_config():
@@ -66,24 +98,54 @@ def create_config():
                 "" if param._default is None else str(param._default),
             )
 
-    with open("luigi.cfg", "w") as f:
+    with open("koopa.cfg", "w") as f:
         config.write(f)
 
 
-def run_pipeline(config_file):
+def set_logging():
+    """Prepare verbose logging for luigi and silence others."""
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+    logging.basicConfig(filename=None, filemode="a", level=logging.ERROR)
+
+    file_handler = logging.FileHandler("koopa.log")
+    file_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        "%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
+    )
+    file_handler.setFormatter(formatter)
+    for log_name in [
+        "luigi-interface",
+        "luigi.scheduler",
+        "luigi",
+        "cellpose",
+        "cellpose.core",
+    ]:
+        logger = logging.getLogger(log_name)
+        logger.addHandler(file_handler)
+
+
+def run_pipeline(config_file, threads):
     """Run standard."""
-    luigi.configuration.add_config_path(config_file)
-    old_config_file = os.path.join(General().analysis_dir, "luigi.cfg")
+    set_logging()
+
+    luigi.configuration.add_config_path(os.path.abspath(config_file))
+    old_config_file = os.path.join(General().analysis_dir, "koopa.cfg")
     if os.path.exists(old_config_file):
         os.remove(old_config_file)
-    luigi.build([SetupPipeline(config_file=config_file), Merge()], local_scheduler=True)
+    luigi.build(
+        [SetupPipeline(config_file=config_file), Merge(threads=threads)],
+        local_scheduler=True,
+        workers=threads,
+    )
 
 
 def main():
     """Run koopa tasks."""
     args = _parse_args()
+
     if args.create_config:
         create_config()
-        return 1
+        sys.exit(1)
 
-    run_pipeline(args.config_file)
+    run_pipeline(args.config, args.workers)
