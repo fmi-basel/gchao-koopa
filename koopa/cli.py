396b6ccc2b76756690da001d20359ba19e2dfe54
Bastian Eichenberger <bastian@eichenbergers.ch>
2022-09-23 11:57:51 +0200

    Refactor CLI and add true silent mode

--- koopa/cli.py
+++ koopa/cli.py
@@ -1,154 +1,49 @@
 """Command line interface."""
 
-import argparse
-import configparser
 import io
-import logging
 import os
 import sys
 
 import luigi
 
 sys.stdout = io.StringIO()
-from . import __version__
+from . import util
+from . import argparse
 from .config import General
-from .config import PreprocessingAlignment
-from .config import PreprocessingNormalization
-from .config import SegmentationOther
-from .config import SegmentationPrimary
-from .config import SegmentationSecondary
-from .config import SpotsColocalization
-from .config import SpotsDetection
-from .config import SpotsTracking
-from .merge import Merge
 from .setup import SetupPipeline
 
 sys.stdout = sys.__stdout__
 
-CONFIGS = [
-    General,
-    PreprocessingAlignment,
-    PreprocessingNormalization,
-    SpotsDetection,
-    SpotsTracking,
-    SpotsColocalization,
-    SegmentationPrimary,
-    SegmentationSecondary,
-    SegmentationOther,
-]
 
-
-def _parse_args():
-    """Basic argument parser."""
-    parser = argparse.ArgumentParser(
-        prog="Koopa",
-        description="Workflow for analysis of cellular microscopy data.",
-        add_help=False,
-    )
-    parser.add_argument(
-        "-c",
-        "--config",
-        type=str,
-        required=False,
-        help="Path to luigi configuration file.",
-    )
-    parser.add_argument(
-        "-w",
-        "--workers",
-        type=int,
-        default=4,
-        help="Number of parallel luigi workers to spawn. [default: 4]",
-    )
-    parser.add_argument(
-        "-cc",
-        "--create-config",
-        action="store_true",
-        help="Create empty configuration file to be passed to `--config`",
-    )
-    parser.add_argument(
-        "-s", "--silent", action="store_true", help="Run koopa silently."
-    )
-    parser.add_argument(
-        "-h",
-        "--help",
-        action="help",
-        default=argparse.SUPPRESS,
-        help="Show this message.",
-    )
-    parser.add_argument(
-        "-V",
-        "--version",
-        action="version",
-        version="%(prog)s " + str(__version__),
-        help="Show %(prog)s's version number.",
-    )
-    if len(sys.argv) == 1:
-        parser.print_help()
-        parser.exit(0)
-    return parser.parse_args()
-
-
-def create_config():
-    """Create a configuration file with default values."""
-    config = configparser.ConfigParser(allow_no_value=True)
-
-    for section in CONFIGS:
-        config.add_section(section.__name__)
-        for name, param in section().get_params():
-            config.set(section.__name__, f"# {name}", param.description)
-            config.set(
-                section.__name__,
-                name,
-                "" if param._default is None else str(param._default),
-            )
-
-    with open("koopa.cfg", "w") as f:
-        config.write(f)
-
-
-def set_logging(silent: bool = False):
-    """Prepare verbose logging for luigi and silence others."""
-    for handler in logging.root.handlers[:]:
-        logging.root.removeHandler(handler)
-    logging.basicConfig(filename=None, filemode="a", level=logging.ERROR)
-
-    file_handler = logging.FileHandler("koopa.log")
-    file_handler.setLevel(logging.ERROR if silent else logging.DEBUG)
-    formatter = logging.Formatter(
-        "%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
-    )
-    file_handler.setFormatter(formatter)
-    for log_name in [
-        "luigi-interface",
-        "luigi.scheduler",
-        "luigi",
-        "cellpose",
-        "cellpose.core",
-    ]:
-        logger = logging.getLogger(log_name)
-        logger.addHandler(file_handler)
-
-
-def run_pipeline(config_file, threads):
-    """Run standard."""
+def setup_configuration(config_file: str, silent: bool):
+    """Replace old configuration file and load parameters into memory."""
     luigi.configuration.add_config_path(os.path.abspath(config_file))
     old_config_file = os.path.join(General().analysis_dir, "koopa.cfg")
     if os.path.exists(old_config_file):
         os.remove(old_config_file)
-    luigi.build(
-        [SetupPipeline(config_file=config_file), Merge(threads=threads)],
-        local_scheduler=True,
-        workers=threads,
-    )
+
+    with util.DisableLogger(silent):
+        luigi.build([SetupPipeline(config_file=config_file)], local_scheduler=True)
+
+
+# def set_environment_variables():
+#     os.environ["CELLPOSE_LOCAL_MODELS_PATH"] = "./"
+#     os.environ["KERAS_HOME"] = "./"
 
 
 def main():
     """Run koopa tasks."""
-    args = _parse_args()
-    set_logging(args.silent)
+    args = argparse._parse_args()
+    util.set_logging()
 
     if args.create_config:
-        create_config()
+        util.create_config()
         sys.exit(1)
 
-    run_pipeline(args.config, args.workers)
+    setup_configuration(args.config, args.silent)
+    tasks = util.create_task_list(args.workers, args.task)
+    if args.force:
+        util.delete_task_outputs(tasks)
+
+    with util.DisableLogger(args.silent):
+        luigi.build(tasks, local_scheduler=True, workers=args.workers)
---
+++ koopa/util.py
@@ -0,0 +1,200 @@
+from typing import List
+import glob
+import configparser
+import logging
+import os
+
+import luigi
+
+from .colocalize import ColocalizeFrame
+from .colocalize import ColocalizeTrack
+from .config import General
+from .config import PreprocessingAlignment
+from .config import PreprocessingNormalization
+from .config import SegmentationCells
+from .config import SegmentationOther
+from .config import SpotsColocalization
+from .config import SpotsDetection
+from .config import SpotsTracking
+from .detect import Detect
+from .merge import Merge
+from .preprocess import Preprocess
+from .registration import ReferenceAlignment
+from .segment_cells import SegmentCells
+from .segment_other import SegmentOther
+from .track import Track
+
+
+CONFIGS = [
+    General,
+    PreprocessingAlignment,
+    PreprocessingNormalization,
+    SpotsDetection,
+    SpotsTracking,
+    SpotsColocalization,
+    SegmentationCells,
+    SegmentationOther,
+]
+
+TASKS = [
+    ColocalizeFrame,
+    ColocalizeTrack,
+    Detect,
+    Preprocess,
+    ReferenceAlignment,
+    SegmentCells,
+    SegmentOther,
+    Track,
+]
+
+TASK_NAMES = [task.__name__ for task in TASKS]
+
+
+def get_file_list():
+    """All file basenames in the image directory."""
+    files = sorted(
+        glob.glob(
+            os.path.join(General().image_dir, "**", f"*.{General().file_ext}"),
+            recursive=True,
+        )
+    )
+    files = [os.path.basename(f).replace(f".{General().file_ext}", "") for f in files]
+    if len(files) != len(set(files)):
+        raise ValueError("Found duplicate file names in image directory.")
+    if not len(files):
+        raise ValueError(
+            f"No files found in directory `{General().image_dir}` "
+            f"with extension `{General().file_ext}`!"
+        )
+    return files
+
+
+def create_config():
+    """Create a configuration file with default values."""
+    config = configparser.ConfigParser(allow_no_value=True)
+
+    for section in CONFIGS:
+        config.add_section(section.__name__)
+        for name, param in section().get_params():
+            config.set(section.__name__, f"# {name}", param.description)
+            config.set(
+                section.__name__,
+                name,
+                "" if param._default is None else str(param._default),
+            )
+
+    with open("koopa.cfg", "w") as f:
+        config.write(f)
+
+
+def remove_all_handlers():
+    """Remove all logging StreamHandlers."""
+    loggers = [logging.getLogger()]
+    loggers = loggers + [
+        logging.getLogger(name) for name in logging.root.manager.loggerDict
+    ]
+
+    for logger in loggers:
+        for handler in logger.handlers:
+            if isinstance(handler, logging.StreamHandler):
+                logger.removeHandler(handler)
+
+
+def set_logging():
+    """Prepare verbose logging for luigi and silence others."""
+    remove_all_handlers()
+
+    formatter = logging.Formatter(
+        "%(asctime)s,%(msecs)d %(levelname)s (%(name)s) - %(message)s",
+        datefmt="%H:%M:%S",
+    )
+    file_handler = logging.FileHandler("koopa.log", mode="a")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.INFO)
+    stream_handler.setFormatter(formatter)
+
+    for log_name in [
+        "cellpose",
+        "cellpose.core",
+        "luigi",
+        "luigi-interface",
+        "luigi.scheduler",
+    ]:
+        logger = logging.getLogger(log_name)
+        logger.addHandler(file_handler)
+        logger.addHandler(stream_handler)
+
+
+def delete_task_outputs(tasks: List[luigi.task.Task]):
+    """Remove all output files to allow rerunning."""
+    output_files = []
+    for task in tasks:
+        output = task.output()
+        if isinstance(output, list):
+            output_files.extend([o.path for o in output])
+        elif isinstance(output, dict):
+            output_files.extend([o.path for o in output.values()])
+        elif isinstance(output, luigi.LocalTarget):
+            output_files.append(output.path)
+        else:
+            raise ValueError(f"Output of undefined format - {type(output)}")
+
+    for file_name in output_files:
+        if os.path.exists(file_name):
+            os.remove(file_name)
+
+
+def create_task_list(threads: int, task_name: str = None) -> List[luigi.task.Task]:
+    """Create a list of all tasks to run."""
+    if task_name is None or task_name == "Merge":
+        return [Merge(threads=threads)]
+
+    if task_name not in TASK_NAMES:
+        raise ValueError(f"Task name invalid. Must be one of {TASK_NAMES}")
+
+    single_task = TASKS[TASK_NAMES.index(task_name)]
+
+    # File independent task(s)
+    if task_name in ("ReferenceAlignment"):
+        return [single_task()]
+
+    # File dependent tasks
+    tasks = []
+    for fname in get_file_list():
+        if task_name in ("SegmentationOther", "Detect", "Track"):
+            enumerator = (
+                SegmentationOther().channels
+                if task_name == "SegmentationOther"
+                else SpotsDetection().channels
+            )
+            tasks.extend(
+                [
+                    single_task(FileID=fname, ChannelIndex=idx)
+                    for idx, _ in enumerate(enumerator)
+                ]
+            )
+        elif task_name in ("ColocalizeTrack", "ColocalizeFrame"):
+            tasks.extend(
+                [
+                    single_task(FileID=fname, ChannelIndex=idx)
+                    for idx, _ in enumerate(SpotsColocalization().channels)
+                ]
+            )
+        else:
+            tasks.append(single_task(FileID=fname))
+    return tasks
+
+
+class DisableLogger:
+    def __init__(self, silent: bool):
+        self.silent = silent
+
+    def __enter__(self):
+        if self.silent:
+            logging.disable(logging.CRITICAL)
+
+    def __exit__(self, exit_type, exit_value, exit_traceback):
+        if self.silent:
+            logging.disable(logging.NOTSET)
