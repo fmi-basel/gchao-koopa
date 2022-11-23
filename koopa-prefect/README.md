4b3807efb9123cc1a5a67cda378707f7df9521a4
Bastian Eichenberger <bastian@eichenbergers.ch>
2022-11-23 15:31:27 +0100

    Move prefect flows to separate repository

--- koopa-prefect/README.md
+++
@@ -1,3 +0,0 @@
-# Koopa-prefect
-
-Workflow logic to integrate core python code from `koopa` and wrap it with a small CLI and prefect web interface.
--- koopa-prefect/src/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import cli
-from . import flows
-from . import tasks_postprocess
-from . import tasks_preprocess
-from . import tasks_segment
-from . import tasks_spots
